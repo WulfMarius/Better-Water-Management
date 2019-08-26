e27beeac7ff72e7b2c1198660a242b2cc5ebb0e4
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-08-26 17:54:36 +0200

    Fix exception when warming up due to drinking hot water

--- VisualStudio/src/UseWaterPatches.cs
+++ VisualStudio/src/UseWaterPatches.cs
@@ -150,17 +150,16 @@ namespace BetterWaterManagement
             {
                 if (!WaterUtils.IsCooledDown(gearItem.m_CookingPotItem))
                 {
-                    GameManager.GetPlayerManagerComponent().ApplyFreezingBuff(20 * progress, 0.5f, 1 * progress);
-                    PlayerDamageEvent.SpawnAfflictionEvent("GAMEPLAY_WarmingUp", "GAMEPLAY_BuffHeader", "ico_injury_warmingUp", InterfaceManager.m_Panel_ActionsRadial.m_FirstAidBuffColor);
+                    GameManager.GetPlayerManagerComponent().ApplyFreezingBuff(20 * progress, 0.5f, 1 * progress, 1);
+                    InterfaceManager.m_Panel_HUD.ShowBuffNotification("GAMEPLAY_WarmingUp", "GAMEPLAY_BuffHeader", InterfaceManager.m_Panel_HUD.m_BuffSpriteFreezingBuff);
                 }
 
                 WaterUtils.SetWaterAmount(gearItem.m_CookingPotItem, waterSupply.m_VolumeInLiters);
                 Object.Destroy(waterSupply);
             }
 
-            if (waterSupply is WaterSourceSupply)
+            if (waterSupply is WaterSourceSupply waterSourceSupply)
             {
-                WaterSourceSupply waterSourceSupply = waterSupply as WaterSourceSupply;
                 waterSourceSupply.UpdateWaterSource();
             }
 
